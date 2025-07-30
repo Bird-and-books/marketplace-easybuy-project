7a0cb5c006698dd6baa60d35decf85395d63b8e3
Oleksandr <collywolly9798@gmail.com>
2025-07-30 19:09:47 +0200

    refactor: replace SCSS styles with TailwindCSS in Footer component

--- src/components/ui/footer-seller/FooterSeller.jsx
+++ src/components/ui/footer-seller/FooterSeller.jsx
@@ -1,14 +1,13 @@
 import SellItemBtn from '@/components/ui/sell-item-btn/SellItemBtn.jsx';
-import style from './FooterSeller.module.scss';
 
 const FooterSeller = () => {
   return (
-    <div className={style.wrapper}>
-      <h2 className={style.title}>Start selling today!</h2>
-      <p className={style.subtitle}>
+    <div className="text-white -mt-18">
+      <h2 className="font-bold text-2xl mb-2">Start selling today!</h2>
+      <p className="font-medium mb-4 max-w-[300px]">
         Join our marketplace and discover new opportunities for your business!
       </p>
-      <ul className={style.list}>
+      <ul className="ml-8 mb-5.5 list-disc">
         <li>Millions of buyers are waiting for your products;</li>
         <li>Increase sales and make more profit;</li>
         <li>User-friendly control panel and growth tools</li>
