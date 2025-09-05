2bed6188b77f17767a3d8b8d3294178b5fa0e55f
NastasiaKarpenko <nastasia.karpenko@gmail.com>
2025-09-05 23:37:24 +0100

    updated some files in branch

--- src/components/home/Recommendations/Recommendations.jsx
+++ src/components/home/Recommendations/Recommendations.jsx
@@ -1,6 +1,6 @@
 import ProductCard from '../ProductCard/ProductCard.jsx';
 import LinkIcon from '@/components/icons/LinkIcon.jsx';
-import './Recommendations.scss';
+import Link from 'next/link';
 
 export default function RecommendationsNew() {
   const products = [
@@ -43,18 +43,29 @@ export default function RecommendationsNew() {
   ];
   return (
     <section className="section-container">
-      <div className="section-wrapper">
-        <h2 className="section-logo">Recommendations</h2>
-        <a className="section-link" href="#">
-          Shop now
-          <LinkIcon />
-        </a>
+      <div className="flex items-center justify-between mb-[24px] lg:mb-[72px] border-b border-dashed ">
+        <h2 className="font-dm font-bold lg:text-[40px]">Recommendations</h2>
+        <Link
+          className='relative inline-flex items-center gap-2 text-[32px] leading-[48px]  transition-colors duration-300 ease-in-out hover:text-blue-600
+         after:content-[""] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-blue-700
+         after:origin-bottom-right after:scale-x-0 hover:after:scale-x-100
+         after:transition-transform after:duration-300 after:ease-in-out '
+          href="#"
+        >
+          <span className="hidden md:inline text-4xl">Shop now</span>
+          <LinkIcon className="transition-transform duration-300 ease-in-out group-hover:translate-x-[5px]" />
+        </Link>
       </div>
-      <div className="product-wrapper">
+      <ul className="flex flex-wrap gap-x-2 gap-y-4 md:flex-nowrap md:gap-x-8 md:gap-y-0">
         {products.map((product, i) => (
-          <ProductCard key={i} {...product} />
+          <li
+            key={i}
+            className="max-w-[167px] bg-white shadow-main relative rounded-2xl md:max-w-[396px]"
+          >
+            <ProductCard key={i} {...product} />
+          </li>
         ))}
-      </div>
+      </ul>
     </section>
   );
 }
